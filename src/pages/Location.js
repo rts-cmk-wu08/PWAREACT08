811a022414d1dc19592de19b7e3d7b11d1ef6a1c
djbenjamin2001 <benjaminbatbayli@gmail.com>
2023-08-23 00:40:55 +0200

    added geolocation

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Location = () => {
-    return ( <h1>this is a location</h1> );
+    const [myPos, setMyPos] = useState();
+    useEffect(() => {
+        if('geolocation' in navigator){
+            navigator.geolocation.getCurrentPosition((position) => {
+                console.log(position)
+                setMyPos(position)
+            })
+        }
+    }, []);
+    return ( <> 
+     <h1>this is a location</h1> 
+     <p>Latitude: {myPos?.coords.latitude}</p>
+     <p>Longitude: {myPos?.coords.longitude}</p>
+     <p>Altitude: {myPos?.coords.altitude}</p>
+     <p>Speed: {myPos?.coords.speed}</p>
+     <p>Accuracy: {myPos?.coords.accuracy}</p>
+    </>
+  
+    );
 }
  
 export default Location;
