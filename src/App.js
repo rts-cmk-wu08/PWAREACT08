00df06cfcb373f2c9a58a42a1c61317259f90723
djbenjamin2001 <benjaminbatbayli@gmail.com>
2023-08-23 01:04:22 +0200

    add watchpos and getpos, nav created too

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import PWAPrompt from 'react-ios-pwa-prompt'
+import { Link } from 'react-router-dom';
 function App() {
   return (
     <>
     <header>
         <p>this is header</p>
+        <nav>
+          <Link to="/">home</Link> &nbsp; &nbsp;
+        <Link to="/location">Location</Link>
+        </nav>
     </header>
     <main>
         <Outlet/>
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const Location = () => {
     const [myPos, setMyPos] = useState();
+    const [dynPos, setDynPos] = useState();
     useEffect(() => {
         if('geolocation' in navigator){
             navigator.geolocation.getCurrentPosition((position) => {
@@ -10,13 +11,34 @@ const Location = () => {
             })
         }
     }, []);
+
+    const handleDynamicPosition = (position) =>{
+        setDynPos(position)
+    }
+    const handleDynamicPositionError = (err) =>{
+      console.log(err)
+    }
+
+    navigator.geolocation.watchPosition(handleDynamicPosition,handleDynamicPositionError, {timeout:5000})
     return ( <> 
-     <h1>this is a location</h1> 
+     <h1>this is static location</h1> 
      <p>Latitude: {myPos?.coords.latitude}</p>
      <p>Longitude: {myPos?.coords.longitude}</p>
      <p>Altitude: {myPos?.coords.altitude}</p>
      <p>Speed: {myPos?.coords.speed}</p>
      <p>Accuracy: {myPos?.coords.accuracy}</p>
+
+     <h1>this is dynamic position</h1>
+    
+       <>
+       <p>Latitude: {dynPos?.coords.latitude}</p>
+     <p>Longitude: {dynPos?.coords.longitude}</p>
+     <p>Altitude: {dynPos?.coords.altitude}</p>
+     <p>Speed: {dynPos?.coords.speed}</p>
+     <p>Accuracy: {dynPos?.coords.accuracy}</p>
+       </>
+
+  
     </>
   
     );
