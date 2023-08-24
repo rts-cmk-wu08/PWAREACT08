33cb2fce08f468a09b55d02920ebb405a3e3d2ad
djbenjamin2001 <benjaminbatbayli@gmail.com>
2023-08-24 10:49:57 +0200

    added iphone support to gyro

--- src/pages/Gyro.js
+++ src/pages/Gyro.js
@@ -1,27 +1,47 @@
 import { useState, useEffect } from "react";
 
 const Gyro = () => {
-    const [deviceAlpha, setDeviceAlpha] = useState();
-    const [deviceBeta, setDeviceBeta] = useState();
-    const [deviceGamma, setDeviceGamma] = useState();
+  const [deviceAlpha, setDeviceAlpha] = useState();
+  const [deviceBeta, setDeviceBeta] = useState();
+  const [deviceGamma, setDeviceGamma] = useState();
+  const [isIOS, setIsIOS] = useState(false);
+  useEffect(() => {
+    if (navigator.userAgent.match(/iPhone/i)) {
+      setIsIOS(true);
+    }
+  }, []);
+  useEffect(() => {
+    window.addEventListener("deviceorientation", (e) => {
+      setDeviceAlpha(e.alpha.toFixed(1));
+      setDeviceBeta(e.beta.toFixed(1));
+      setDeviceGamma(e.gamma.toFixed(1));
+    });
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener("deviceorientation", e =>{
-            setDeviceAlpha(e.alpha.toFixed(1))
-            setDeviceBeta(e.beta.toFixed(1))
-            setDeviceGamma(e.gamma.toFixed(1))
-        })
-    }, []);
-    return ( 
-<>
-<h1>Gyroscope</h1>
+  const enableDeviceOrientation = () =>{
+    DeviceOrientationEvent.requestPermission().then(response => {
+        if(response === "granted"){
+            window.addEventListener("deviceorientation", (e) => {
+                setDeviceAlpha(e.alpha.toFixed(1));
+                setDeviceBeta(e.beta.toFixed(1));
+                setDeviceGamma(e.gamma.toFixed(1));
+              });
+        }
+    })
+  }
+  return (
+    <>
+      <h1>Gyroscope</h1>
+      {isIOS && <p>Du er på en dårlig apple produkt/iphone</p>}
+      <h2>Værdier fra gyroskopet:</h2>
+      <p>alpha: {deviceAlpha}</p>
+      <p>beta:{deviceBeta}</p>
+      <p>gamma:{deviceGamma}</p>
+      {isIOS && !deviceAlpha ? (
+        <button onClick={enableDeviceOrientation}>Enable on Iphone</button>
+      ) : null}
+    </>
+  );
+};
 
-<h2>Værdier fra gyroskopet:</h2>
-<p>alpha: {deviceAlpha}</p>
-<p>beta:{deviceBeta}</p>
-<p>gamma:{deviceGamma}</p>
-</>
-     );
-}
- 
 export default Gyro;
